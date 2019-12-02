f1496f22bf92eadad854dfd5366cd6df59f7b63a
simon <simon@LAPTOP-R5AVCR39>
2019-12-02 15:06:08 +0100

    La till koden för player.

--- src/videopoker/Player.java
+++ src/videopoker/Player.java
@@ -1,5 +1,47 @@
 package videopoker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
-	// Player
+	private ArrayList<Card> hand;
+	private int wallet=0;
+	
+	public Player() {
+		hand = new ArrayList<Card>();
+		wallet = 0;
+	}
+	
+	
+	public void addMoney(int money) {
+		this.wallet+=money;
+	}
+	public boolean withdrawMoney(int withdrawMoney) {
+		
+		if(wallet>withdrawMoney) {
+			wallet+=-withdrawMoney;
+			return true;
+		}
+		return false;
+	}
+	
+	public int checkWallet() {
+		return wallet;
+	}
+	
+	public void addCardToHand(Card card) {
+		hand.add(card);
+	}
+	public void removeCardFromHand(Card card) {
+		hand.remove(card);
+	}
+	
+	public List<Card> getHand() {
+		return hand;
+	}
+	
+	public void reset() {
+		hand.clear();
+	}
+
 }
