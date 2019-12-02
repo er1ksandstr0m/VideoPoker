2c1020ce5d533d3bfe0ca8d224f6af84837fde31
Pontus <bollpiska@hotmail.com>
2019-12-02 15:58:40 +0100

    lade till bland annat en bytKort-metod i Videopoker. Även några instansvariabler.

--- src/videopoker/VideoPoker.java
+++ src/videopoker/VideoPoker.java
@@ -1,7 +1,61 @@
 package videopoker;
 
 public class VideoPoker {
-	// VideoPlayer
-	
-	//Testar push 
+	Player spelare;
+	Deck kortlek;
+
+	public VideoPoker(){
+
+	  spelare = new Player();
+	  kortlek = new Deck();
+
+		kortlek.shuffle();
+
+	  for(int i = 0; i <5 ; i++){
+	    spelare.addCardToHand(kortlek.draw());
+	}
+
+	  bytKort();
+
+
+	}
+
+
+	public String handTillSträng(ArrayList<Card> hand){
+		String handTillSträng = "":
+	  for(Card kort = spelare.getHand()){
+	    handTillSträng += kort.toString() + ", ";
+	  }
+		return handTillSträng;
+
+	}
+
+
+	public void bytKort(){
+
+
+	  Scanner sc = new Scanner(System.in);
+
+	  System.out.println("Du har korten " + handTillSträng(spelare.getHand()) +
+	  " Vilka kort vill du byta (skriv kortets nummer)");
+
+
+	  while(sc.hasNextInt()){
+	    spelare.changeCard(spelare.getHand().get(sc.nextInt()-1), kortlek.draw());
+	  }
+
+
+		System.out.println("Du fick korten " + handTillSträng(spelare.getHand()) +
+		" och fick " + score() + " poäng";)
+
+
+	  sc.close();
+	  }
+
+
+	  public static void main(String []args){
+			VideoPoker poker = new VideoPoker();
+	  }
+	}
+
 }
