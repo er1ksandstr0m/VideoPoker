df5cbbf5adc0526eb3e49b78b84f6bbea24f12a8
Simon Södergren <simon.sodergren@live.se>
2019-12-02 13:43:04 +0100

    Jag har kopierat över från blackjack.

--- src/videopoker/Card.java
+++ src/videopoker/Card.java
@@ -1,5 +1,23 @@
 package videopoker;
 
+
 public class Card {
-	// Card
+
+	private int value;
+	private Suit suit;
+
+	public Card(int value, Suit suit) {
+		this.value = value;
+		this.suit = suit;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public Suit getSuit() {
+		return suit;
+	}
 }
+
+
--- src/videopoker/Deck.java
+++ src/videopoker/Deck.java
@@ -1,5 +1,38 @@
 package videopoker;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Deck {
-	// Deck
+	private List<Card> cards = new ArrayList<>();
+
+	public Deck() {
+		for (int i = 0; i < Suit.values().length; i++) {
+			for (int j = 1; j < 14; j++) {
+				cards.add(new Card(j, Suit.values()[i]));
+			}
+		}
+	}
+
+	public Card draw() {
+		Card card = cards.get(0);
+		cards.remove(card);
+		return card;
+	}
+
+	public void shuffle() {
+		List<Card> shuffledCards = new ArrayList<>();
+		Random random = new Random();
+		Card card;
+
+		while (cards.size() > 0) {
+			card = cards.get(random.nextInt(cards.size()));
+			shuffledCards.add(card);
+			cards.remove(card);
+		}
+
+		cards = shuffledCards;
+	}
 }
+
