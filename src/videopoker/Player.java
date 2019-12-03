9a34f929043c1c24eabfdfdb6423e244fe6819a2
simon <simon@LAPTOP-R5AVCR39>
2019-12-03 15:10:31 +0100

    La in koden för score i en metod. La till funktioner och dokumentation för player

--- src/videopoker/Player.java
+++ src/videopoker/Player.java
@@ -1,45 +1,95 @@
 package videopoker;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Player {
 	private ArrayList<Card> hand;
-	private int wallet=0;
-	
+	private int wallet = 0;
+
+	/**
+	 * Skapar spelare med tom hand och plånbok.
+	 */
 	public Player() {
 		hand = new ArrayList<Card>();
 		wallet = 0;
 	}
 	
-	
-	public void addMoney(int money) {
-		this.wallet+=money;
+	/**
+	 * Skapar spelare med gammal plånbok och hand.
+	 * @param oldWallet
+	 * @param oldHand
+	 */
+	public Player(int oldWallet, ArrayList<Card>oldHand) {
+		hand = new ArrayList<Card>();
+		wallet = 0;
 	}
+
+	
+
+	/**
+	 * Lägg till pengar i plånboken
+	 * @param money
+	 */
+	public void addMoney(int money) {
+		this.wallet += money;
+	}
+
+	/**
+	 * Gör försök att ta ut pengar. Returnerar om uttaget går bra eller ej.
+	 * @param withdrawMoney
+	 * @return true om spelaren har råd med uttaget, annars false.
+	 */
 	public boolean withdrawMoney(int withdrawMoney) {
-		
-		if(wallet>withdrawMoney) {
-			wallet+=-withdrawMoney;
+
+		if (wallet > withdrawMoney) {
+			wallet += -withdrawMoney;
 			return true;
 		}
 		return false;
 	}
-	
+
+	/**
+	 * Returnerar plånboken.
+	 * @return wallet
+	 */
 	public int checkWallet() {
 		return wallet;
 	}
-	
+
+	/**
+	 * Lägger till ett kort till handen.
+	 * @param card Det nya kortet
+	 */
 	public void addCardToHand(Card card) {
 		hand.add(card);
 	}
+	
+	/**
+	 * Byter ut ett kort i handen till ett nytt kort.
+	 * @param oldCard Kortet som ska bytas ut
+	 * @param newCard Kortet som ska bytas in
+	 */
+	public void changeCard(Card oldCard, Card newCard) {
+		hand.set(hand.indexOf(oldCard), newCard);
+	}
+
+	/**
+	 * @param card
+	 */
 	public void removeCardFromHand(Card card) {
 		hand.remove(card);
 	}
-	
-	public List<Card> getHand() {
+
+	/**
+	 * @return
+	 */
+	public ArrayList<Card> getHand() {
 		return hand;
 	}
-	
+
+	/**
+	 * Tömmer spelarens hand
+	 */
 	public void reset() {
 		hand.clear();
 	}
--- src/videopoker/VideoPoker.java
+++ src/videopoker/VideoPoker.java
@@ -8,32 +8,29 @@ public class VideoPoker {
 	Player spelare;
 	Deck kortlek;
 
-	public VideoPoker(){
+	public VideoPoker() {
 
-	  spelare = new Player();
-	  kortlek = new Deck();
+		spelare = new Player();
+		kortlek = new Deck();
 
 		kortlek.shuffle();
 
-	  for(int i = 0; i <5 ; i++){
-	    spelare.addCardToHand(kortlek.draw());
+		for (int i = 0; i < 5; i++) {
+			spelare.addCardToHand(kortlek.draw());
+		}
+
+		bytKort();
+
 	}
 
-	  bytKort();
-
-
-	}
-
-
-	public String handTillSträng(ArrayList<Card> hand){
+	public String handTillSträng(ArrayList<Card> hand) {
 		String handTillSträng = "";
-	  for(Card kort = spelare.getHand()){
-	    handTillSträng += kort.toString() + ", ";
-	  }
+		for (Card kort : spelare.getHand()) {
+			handTillSträng += kort.toString() + ", ";
+		}
 		return handTillSträng;
 
 	}
-
 
 	public void bytKort(){
 
@@ -50,50 +47,50 @@ public class VideoPoker {
 
 
 		System.out.println("Du fick korten " + handTillSträng(spelare.getHand()) +
-		" och fick " + score() + " poäng";)
+		" och fick " + /*score() +*/ " poäng");
 
 
 	  sc.close();
 	  }
 
-
-	  public static void main(String []args){
-			VideoPoker poker = new VideoPoker();
-	  }
-	}
-	//SCORE
+	public void score() {
+		// SCORE
 		String s = "";
 		int p = 0;
 
-		int[] array = {1, 3, 2, 2, 1};
+		int[] array = { 1, 3, 2, 2, 1 };
 		Arrays.sort(array);
-		for(int i = 0; i < array.length; i++) {
+		for (int i = 0; i < array.length; i++) {
 			System.out.println(array[i]);
 		}
-		for(int i = 0; i < 4; i++) {
-			if(array[i] == array[i+1]) {
+		for (int i = 0; i < 4; i++) {
+			if (array[i] == array[i + 1]) {
 				s += "Par";
 			} else {
 				s += "Null";
 			}
 
 		}
-		if((array[4]) == (array[0]+4)){
+		if ((array[4]) == (array[0] + 4)) {
 			s += "Stege";
 		}
 		System.out.println(s);
 
-
-		if(s.contains("Stege") && !s.contains("Par")) {
-		System.out.println("Stege!");
+		if (s.contains("Stege") && !s.contains("Par")) {
+			System.out.println("Stege!");
 		} else if (s.contains("ParParPar")) {
-		System.out.println("Fyrtal!");
-		} else if(s.contains("ParPar") && s.startsWith("Par") && ((s.endsWith("Par")||(s.endsWith("ParStege"))))){
-		System.out.println("Kåk!");
-		} else if(s.contains("ParPar") && ((s.contains("NullNull"))||((s.startsWith("Null") && ((s.endsWith("Null")||s.endsWith("NullStege"))))))){
-		System.out.println("Triss!");
-		} else if ((s.matches("ParNullParNull"))||(s.matches("ParNullNullPar"))||(s.matches("NullParNullPar"))) {
-		System.out.println("Två Par!");
+			System.out.println("Fyrtal!");
+		} else if (s.contains("ParPar") && s.startsWith("Par") && ((s.endsWith("Par") || (s.endsWith("ParStege"))))) {
+			System.out.println("Kåk!");
+		} else if (s.contains("ParPar") && ((s.contains("NullNull"))
+				|| ((s.startsWith("Null") && ((s.endsWith("Null") || s.endsWith("NullStege"))))))) {
+			System.out.println("Triss!");
+		} else if ((s.matches("ParNullParNull")) || (s.matches("ParNullNullPar")) || (s.matches("NullParNullPar"))) {
+			System.out.println("Två Par!");
 		}
+	}
 
+	public static void main(String[] args) {
+		VideoPoker poker = new VideoPoker();
+	}
 }
