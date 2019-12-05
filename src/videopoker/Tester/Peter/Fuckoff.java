c37f31b5f1fd79ab5e16ceb7da10dd7af12ed10e
OnlySchnitzel <peter.ihm98@gmail.com>
2019-12-05 14:26:13 +0100

    JAG ÄR DONE FÖR IDAG: LA TILL SAKER I POM FÖR GSON OCH SCRIPT

---
+++ src/videopoker/Tester/Peter/Fuckoff.java
@@ -0,0 +1,16 @@
+package videopoker.Tester.Peter;
+
+
+public class Fuckoff
+{
+    public String name;
+    public Integer age;
+    public Fuckofftwo two = new Fuckofftwo();
+
+    public Fuckoff()
+    {
+        name = "Peter";
+        age = 21;
+        two.hasGivenUpOnLife = false;
+    }
+}
--- src/videopoker/Tester/Peter/savertest.java
+++ src/videopoker/Tester/Peter/savertest.java
@@ -1,96 +1,48 @@
 package videopoker.Tester.Peter;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.net.URL;
-import java.util.Scanner;
 
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.WindowConstants;
-
-import org.json.JSONArray;
-import org.json.JSONException;
+import com.google.gson.Gson;
 
 public class savertest {
 
-	JFrame frame;
-	JComboBox<Integer> combo;
-	JSONArray arr;
-	JLabel label = new JLabel();
+	public static Fuckoff foff = new Fuckoff();
+	private static String path = "SaveFile.json";
 
-	public static void main(String[] args) {
-		// http://data.goteborg.se/SelfServiceBicycleService/v1.0/Stations/40f6e365-e959-43d7-92be-964ee2f88a09?format=json
-		new savertest();
+	public static void main(String[] args) throws IOException {
+
+		Fuckoff newfoff = new Fuckoff();
+		Load(newfoff);
+		System.out.println(newfoff.name);
+        System.out.println(newfoff.age);
+		System.out.println(newfoff.two);
+		System.out.println(newfoff.two.idk);
+        System.out.println(newfoff.two.hasGivenUpOnLife);
 		
 	}
 
-	public savertest() {
-		frame = new JFrame();
-		frame.setSize(850, 675);
-		getData(1);
+	public static void Save(Fuckoff test) throws IOException {
+		Gson gson = new Gson();
+		String json = gson.toJson(test);
 
-		frame.setLayout(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
-
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridy = 0;
-		c.gridx = 0;
-		Integer[] stationIDs = new Integer[arr.length()];
-
-		for (int i = 0; i < arr.length(); i++) {
-			stationIDs[i] = (Integer) arr.getJSONObject(i).get("StationId");
-		}
-		combo = new JComboBox<Integer>(stationIDs);
-
-		frame.add(combo);
-		c.gridy = 1;
-		updateLabel();
-		combo.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				updateLabel();
-			}
-		});
-		
-		frame.add(label);
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-	}
-
-	private void updateLabel() {
-		combo.getSelectedItem();
-		// Name AvailableBikes BikeStands
-		String text = "";
-		for (int i = 0; i < arr.length(); i++) {
-			if ((Integer) arr.getJSONObject(i).get("StationId") == (Integer) combo.getSelectedItem()) {
-				text = "<html><br>Namn: " + arr.getJSONObject(i).getString("Name") + "<br>Antal platser: "
-						+ arr.getJSONObject(i).get("BikeStands") + "<br>Tillgängliga cyklar: ";
-				try {
-					text += arr.getJSONObject(i).get("AvailableBikes");
-				} catch (JSONException e) {
-					text += 0;
-				}
-			}
-		}
-		label.setText(text);
-
-	}
-
-	@SuppressWarnings("resource")
-	public void getData(int id) {
-		String url = "http://data.goteborg.se/SelfServiceBicycleService/v1.0/Stations/40f6e365-e959-43d7-92be-964ee2f88a09?format=json";
-		String json = "";
 		try {
-			json = new Scanner(new URL(url).openStream(), "UTF-8").nextLine();
-		} catch (IOException e) {
-			e.printStackTrace();
+			FileWriter writer = new FileWriter(path);
+			writer.write(json);
+			writer.close();
+		} catch (Exception e) {
+			System.out.println(e);
 		}
-		arr = new JSONArray(json);
-
+		System.out.println("Success...");
 	}
 
+	public static void Load(Fuckoff blabla) throws FileNotFoundException
+	{
+		FileReader reader = new FileReader(path);
+		Gson gson = new Gson();
+		blabla = gson.fromJson(reader.toString(), Fuckoff.class);
+	}
 }
