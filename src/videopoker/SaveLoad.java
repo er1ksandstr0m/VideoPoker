4674565f9abca6e1c2f4d1adbedec7e98b78003d
OnlySchnitzel <peter.ihm98@gmail.com>
2019-12-06 10:03:11 +0100

    Lagt till saker i SaveLoad()

--- src/videopoker/SaveLoad.java
+++ src/videopoker/SaveLoad.java
@@ -1,14 +1,38 @@
 package videopoker;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import com.google.gson.Gson;
+
 public class SaveLoad 
 {
-	public void Save()
+	private String path = "SaveGame.json";
+
+	public void Save(VideoPoker test)
 	{
-		
+		Gson gson = new Gson();
+		String json = gson.toJson(test);
+
+		try {
+			FileWriter writer = new FileWriter(path);
+			writer.write(json);
+			writer.close();
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 
-	public void Load()
+	public VideoPoker Load()
 	{
-		
+		try {
+			Gson gson = new Gson();
+			return gson.fromJson(new FileReader(path), VideoPoker.class);
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+
+		return new VideoPoker();
 	}
 }
