46adcd805f98b29ac28bfcd2c6062c572af08775
OnlySchnitzel <peter.ihm98@gmail.com>
2019-12-09 11:18:29 +0100

    Lagt till lite kommentarer och ändrade på Load funktionen eftersom vi la till en Player i videopoker

--- src/videopoker/SaveLoad.java
+++ src/videopoker/SaveLoad.java
@@ -6,13 +6,20 @@ import com.google.gson.Gson;
 
 public class SaveLoad 
 {
+	//Filepath där json ska sparas
 	private String path = "SaveGame.json";
 
+	//Load för att ladda spelet
+	//använder Gson som är googles egna Json grej
+	//kör en try catch för att skriva till object
+	//skapar en FileWriter som skriver till filen
 	public void Save(VideoPoker test)
 	{
 		Gson gson = new Gson();
 		String json = gson.toJson(test);
 
+
+		
 		try {
 			FileWriter writer = new FileWriter(path);
 			writer.write(json);
@@ -23,8 +30,16 @@ public class SaveLoad
 		}
 	}
 
+	//Ladda in Json objectet
+	//använder try och catch precis som i save funktionen
+	//ifall ett object hittades så laddar den in datan från Gson
+	//annars så returnar den en ny Videopoker och Player eftersom den 
+	//utgår ifrån att det inte finns något
+	//save game.
+
 	public VideoPoker Load()
 	{
+
 		try {
 			Gson gson = new Gson();
 			return gson.fromJson(new FileReader(path), VideoPoker.class);
@@ -33,6 +48,6 @@ public class SaveLoad
 			System.out.println(e);
 		}
 
-		return new VideoPoker();
+		return new VideoPoker(new Player());
 	}
 }
